60b669c69dc46246956f9f9cab03c2a0fdae4dd0
grsmith72 <39013311+grsmith72@users.noreply.github.com>
2018-05-05 15:48:38 -0500

    my WPF RPG project
    upload of my project solution and files for Scott

---
+++ SOSCSRPG/Engine/Models/GameItem.cs
@@ -0,0 +1,23 @@
+﻿namespace Engine.Models
+{
+    public class GameItem
+    {
+        public int ItemTypeID { get; }
+        public string Name { get; }
+        public int Price { get; }
+        public bool IsUnique { get; }
+
+        public GameItem(int itemTypeID, string name, int price, bool isUnique = false)
+        {
+            ItemTypeID = itemTypeID;
+            Name = name;
+            Price = price;
+            IsUnique = isUnique;
+        }
+
+        public GameItem Clone()
+        {
+            return new GameItem(ItemTypeID, Name, Price, IsUnique);
+        }
+    }
+}
